cd5f4f50f2346d26745b024769319620437328e5
Thedarkmoon28 <mario.02.mav@gmail.com>
2026-03-11 17:56:47 -0600

    Header commit

--- app/src/main/java/com/example/financeapp/components/Header.kt
+++ app/src/main/java/com/example/financeapp/components/Header.kt
@@ -1,2 +1,39 @@
 package com.example.financeapp.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun Header(userName: String) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Column {
+
+            Text(
+                text = "Hola $userName",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text("Bienvenido")
+        }
+
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = "Menu"
+        )
+    }
+}
